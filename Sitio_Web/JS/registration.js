c3232e5359eee10f289e697cc2f43ab06ae5c474
Darlene Navarro <dar.navarror@duocuc.cl>
2021-06-29 23:09:58 -0400

    Interfaz: Login y Registrarse listo

--- Sitio_Web/JS/registration.js
+++ Sitio_Web/JS/registration.js
@@ -1,3 +1,39 @@
+$(document).ready(function () {
+    /* Servicios Extras */
+    $('#form-registration').validate({
+        rules: {
+            num_rut: {required: true, minlength: 7, maxlength: 13},
+            dv_rut: {required: true, maxlength: 1},
+            nombre: {required: true, minlength: 3, maxlength: 40},
+            apellido: {required: true, minlength: 5, maxlength: 40},
+            fec_nac: {required: true},
+            email: {required: true, minlength: 13, maxlength: 30},
+            username: {required: true, minlength: 5, maxlength: 20},
+            pass: {required: true, minlength:7, maxlength: 10}
+        },
+        messages: {
+            num_rut: "El campo Número Rut es obligatorio.",
+            dv_rut: "El campo Dígito Verificador es obligatorio.",
+            nombre: "El campo Nombre es obligatorio.",
+            apellido: "El campo Apellidos es obligatorio.",
+            fec_nac: "El campo Fecha de Nacimiento es obligatorio.",
+            email: "El campo Email es obligatorio.",
+            username: "El campo Nombre de Usuario es obligatorio.", 
+            pass: "El campo Constraseña es obligatorio."
+        },
+        errorElement : 'span',
+
+    });
+    //Redirección a página anterior
+    $('#volver').click(function(){
+        location.href = "/PRY3111-Hotel_Veranum/Sitio_Web/index.html";
+    });
+});
+
+
+
+
+/* Animación para Sección de Ingreso */
 $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     var $this = $(this),
         label = $this.prev('label');
